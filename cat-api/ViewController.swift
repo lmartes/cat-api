9718a71cdcb3b238ba0d4532c3a83e749ae24c40
Leonardo Martes <lmartes@wawand.co>
2021-12-09 09:35:04 -0500

    Static variables are added with the api key and a model folder is created.

--- cat-api/ViewController.swift
+++ cat-api/ViewController.swift
@@ -1,10 +1,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    static var apiKey = "api_key=f227d41d-60de-4c96-b588-2cdba266e0ba"
+    static var url = "https://api.thecatapi.com/v1/images/search"
+    
+    @IBOutlet weak var breedTitle: UILabel!
+    @IBOutlet weak var breedImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    private func requestCatAPI() {
     }
 
     @IBAction func actionLike(_ sender: Any) {
