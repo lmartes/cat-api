72bb2b0077d56f11e50a86822abd49752bb8a10e
Leonardo Martes <lmartes@wawand.co>
2021-12-09 17:04:24 -0500

    * ObjectMapper is added. * The main request is made.

--- cat-api/Controllers/ViewController.swift
+++ cat-api/Controllers/ViewController.swift
@@ -1,4 +1,6 @@
 import UIKit
+import Alamofire
+import ObjectMapper
 
 class ViewController: UIViewController {
     @IBOutlet weak var breedTitle: UILabel!
@@ -9,6 +11,34 @@ class ViewController: UIViewController {
     }
 
     @IBAction func actionLike(_ sender: Any) {
+        requestCatAPI()
+    }
+    
+    func requestCatAPI() {
+        let url = "https://api.thecatapi.com/v1/images/search?api_key=f227d41d-60de-4c96-b588-2cdba266e0ba"
+        performRequest(url)
+    }
+    
+    func performRequest(_ urlString: String) {
+        AF.request(urlString).responseJSON { (response) in
+            if let error = response.error {
+                print("Error: ", error)
+            }
+            
+            if let data = response.data {
+                self.parseJSON(data)
+            }
+        }
+    }
+    
+    func parseJSON(_ data: Data) {
+        let str = String(decoding: data, as: UTF8.self)
+        guard let cats = Mapper<CatAPIData>().mapArray(JSONString: str) else {
+            return
+        }
+        
+        let firstID = cats.first?.getID()
+        print("ID: ", firstID)
     }
     
     @IBAction func actionDislike(_ sender: Any) {
--- cat-api/Model/CatAPIModel.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-struct CatAPIModel {
-    private var date: Date = Date()
-    private var name: String = ""
-    private var typeVote: String = ""
-}
