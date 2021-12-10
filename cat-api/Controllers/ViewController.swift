77d5cdc9c2fac28759664ba8109cfd59787681dc
Leonardo Martes <lmartes@wawand.co>
2021-12-10 00:26:05 -0500

    The SVProgressHUD pod is installed.

--- cat-api/Controllers/ViewController.swift
+++ cat-api/Controllers/ViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Alamofire
 import ObjectMapper
+import SVProgressHUD
 
 class ViewController: UIViewController {
     @IBOutlet weak var breedTitle: UILabel!
@@ -15,8 +16,9 @@ class ViewController: UIViewController {
     func requestCatAPI() {
         //let apiKey = "api_key=f227d41d-60de-4c96-b588-2cdba266e0ba"
         let url = "https://api.thecatapi.com/v1/breeds?limit=20&page=0"
-        
+        SVProgressHUD.show()
         AF.request(url).responseJSON { (response) in
+            SVProgressHUD.dismiss()
             if let error = response.error {
                 print("Error: ", error)
             }
