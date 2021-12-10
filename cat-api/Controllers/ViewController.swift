099b0a4c5fd01e81a8d7cbdfc61ef7c6913d1384
Leonardo Martes <lmartes@wawand.co>
2021-12-10 00:16:02 -0500

    A method is created to update the screen.

--- cat-api/Controllers/ViewController.swift
+++ cat-api/Controllers/ViewController.swift
@@ -5,20 +5,18 @@ import ObjectMapper
 class ViewController: UIViewController {
     @IBOutlet weak var breedTitle: UILabel!
     @IBOutlet weak var breedImage: UIImageView!
+    private var catsByBreed: [CatBreedResponse] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         requestCatAPI()
     }
-
-    func requestCatAPI() {
-        let apiKey = "api_key=f227d41d-60de-4c96-b588-2cdba266e0ba"
-        let url = "https://api.thecatapi.com/v1/breeds?limit=20"
-        performRequest(url)
-    }
     
-    func performRequest(_ urlString: String) {
-        AF.request(urlString).responseJSON { (response) in
+    func requestCatAPI() {
+        //let apiKey = "api_key=f227d41d-60de-4c96-b588-2cdba266e0ba"
+        let url = "https://api.thecatapi.com/v1/breeds?limit=20&page=0"
+        
+        AF.request(url).responseJSON { (response) in
             if let error = response.error {
                 print("Error: ", error)
             }
@@ -31,24 +29,39 @@ class ViewController: UIViewController {
     
     func parseJSON(_ data: Data) {
         let str = String(decoding: data, as: UTF8.self)
-        guard let cat = Mapper<CatBreedResponse>().mapArray(JSONString: str) else {
+        guard let cats = Mapper<CatBreedResponse>().mapArray(JSONString: str) else {
             return
         }
         
-        let breedName = cat.first?.getName()
+        catsByBreed = cats
+        DispatchQueue.main.async {
+            self.updateUI()
+        }
+    }
+    
+    func updateUI() {
+        if catsByBreed.count <= 0 {
+            return
+        }
+        
+        let breedName = catsByBreed.first?.getName()
         breedTitle.text = breedName
         
-        guard let breedImageURL = cat.first?.getCatBreedImage().getURL(), let data = try? Data(contentsOf: breedImageURL) else {
+        guard let breedImageURL = catsByBreed.first?.getCatBreedImage().getURL(), let data = try? Data(contentsOf: breedImageURL) else {
             return
         }
         self.breedImage.image = UIImage(data: data)
+        
+        catsByBreed.removeFirst()
     }
     
 // MARK: - IBActions
     @IBAction func actionLike(_ sender: Any) {
+        updateUI()
     }
     
     @IBAction func actionDislike(_ sender: Any) {
+        updateUI()
     }
 }
 
